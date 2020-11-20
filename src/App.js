7e1c9eac07bf36b64234ed571462b2c5319f6e0d
Kaeneth Dayao <kendayao@gmail.com>
2020-11-20 14:48:13 -0800

    post component styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import Post from './post/Post'
 
 function App() {
+
+  
   return (
     <div className="app">
       <div className="app__header">
       <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram logo" />
      </div>
 
-    <Post />
-
+    <Post username='Kendayao' caption='Lets begin the day' imageUrl='https://images.unsplash.com/photo-1517404215738-15263e9f9178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'/>
+   
 
 
 
--- src/post/Post.js
+++ src/post/Post.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Post.css';
+import Avatar from '@material-ui/core/Avatar';
 
-function Post(){
+function Post({username, caption, imageUrl}){
     return (
-        <div>
-        <h3>h3</h3>
+        <div className="post">
+            <div className="post__header">
+                <Avatar 
+                    className="post__avatar"
+                    alt={username}
+                    src="/static/images/avatar/1.jpg"
+                />
+                <h3>{username}</h3>
+            </div>
+            <img className="post__image" src={imageUrl} />
+            <h4 className="post__text"><strong>{username}:</strong> {caption}</h4>
         </div>
+       
     )
 }
 
