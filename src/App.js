4978c2b2476f8afd1b0245c96785a5eaf87d3886
Kaeneth Dayao <kendayao@gmail.com>
2020-11-20 12:39:24 -0800

    created header and started Post component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
 import './App.css';
+import Post from './post/Post'
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app">
+      <div className="app__header">
+      <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram logo" />
+     </div>
+
+    <Post />
+
+
+
+
+
     </div>
   );
 }
---
+++ src/post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Post.css';
+
+function Post(){
+    return (
+        <div>
+        <h3>h3</h3>
+        </div>
+    )
+}
+
+export default Post;
