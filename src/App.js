feb2acbd8959425b87565f360fdaf7a6e4791d37
Kaeneth Dayao <kendayao@gmail.com>
2020-11-20 19:37:48 -0800

    user auth setupwith email and password

--- src/App.js
+++ src/App.js
@@ -1,14 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Post from './post/Post';
-import {db} from './firebase/firebase'
+import {db, auth} from './firebase/firebase'
+import Modal from '@material-ui/core/Modal'
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Input } from '@material-ui/core';
+
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+
+
 
 function App() {
-
-  const[posts, setPosts]=useState([])
+  const classes=useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [posts, setPosts]=useState([]);
+  const [open, setOpen]=useState(false);
+  const [username, setUsername]=useState('');
+  const [email, setEmail]=useState('');
+  const [password, setPassword]=useState('');
+  const [user, setUser]=useState(null)
 
   useEffect(()=>{
+    const unsubscribe=auth.onAuthStateChanged((authUser)=>{
+      if(authUser){
+        // user has logged in
+        console.log(authUser)
+        setUser(authUser);
+      }else{
+        // user has logged out
+        setUser(null);
+      }
 
+    })
+
+    return()=>{
+      unsubscribe();
+    }
+  },[user, username])
+
+  useEffect(()=>{
     db.collection('posts').onSnapshot(snapshot=>{
       setPosts(snapshot.docs.map(doc=>({
         id: doc.id,
@@ -18,20 +70,79 @@ function App() {
     })
   },[])
   
-  console.log(posts)
+  function signUp(event){
+    event.preventDefault();
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((authUser)=>{
+      return authUser.user.updateProfile({
+        displayName:username,
+      })
+    })
+    .catch((error)=>console.log(error.message))
+  }
   
+  console.log(user)
   return (
     <div className="app">
+      <Modal
+      className="app__modal"
+        open={open}
+        // on close listens to clicks outside the modal. materialize built that for us
+        onClose={()=>setOpen(false)}
+      >
+         <div style={modalStyle} className={classes.paper}>
+           <form className="app__signup">
+            <center>
+              <img className="app__header-imageModal" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram logo" />
+              
+            </center>
+            <center>
+            <img className="app__header-iconModal" src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-instagram-new-circle-512.png" alt="instagram logo" />
+            </center>
+            
+            <Input 
+                className="app__input"
+                placeholder='username'
+                type='username'
+                value={username}
+                onChange={event=>setUsername(event.target.value)}
+                required
+              />
+              <Input
+                className="app__input" 
+                placeholder='email'
+                type='email'
+                value={email}
+                onChange={event=>setEmail(event.target.value)}
+                required
+              />
+              <Input
+                className="app__input" 
+                placeholder='password'
+                type='password'
+                value={password}
+                onChange={event=>setPassword(event.target.value)}
+                required
+              />
+              <Button type="submit" onClick={signUp}>Sign Up</Button>
+           </form>
+         </div>
+      </Modal>
+
       <div className="app__header">
       <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram logo" />
      </div>
+
+    {user?<Button onClick={()=>auth.signOut()}>Logout</Button>:
+    <Button onClick={()=>setOpen(true)}>Sign Up</Button>
+    
+    }
+
     {posts.map(({id, post})=>(
       <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
     ))}
     
-   
-
-
+  
 
 
     </div>
