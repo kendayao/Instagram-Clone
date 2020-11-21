bbee48401f6f6f4abd82709e26fccdd8e7ab1b5a
Kaeneth Dayao <kendayao@gmail.com>
2020-11-20 16:55:40 -0800

    completed firebase integration to grab documents from firestore and display on post component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Post from './post/Post'
+import Post from './post/Post';
+import {db} from './firebase/firebase'
 
 function App() {
 
+  const[posts, setPosts]=useState([])
+
+  useEffect(()=>{
+
+    db.collection('posts').onSnapshot(snapshot=>{
+      setPosts(snapshot.docs.map(doc=>({
+        id: doc.id,
+        post:doc.data()
+        
+      })));
+    })
+  },[])
+  
+  console.log(posts)
   
   return (
     <div className="app">
       <div className="app__header">
       <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram logo" />
      </div>
-
-    <Post username='Kendayao' caption='Lets begin the day' imageUrl='https://images.unsplash.com/photo-1517404215738-15263e9f9178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'/>
+    {posts.map(({id, post})=>(
+      <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+    ))}
+    
    
 
 
